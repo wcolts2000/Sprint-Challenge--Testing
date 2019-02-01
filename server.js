b583a537f82684c4ae7ba72f373ba2dec33c7a86
wcolts2000 <wcolts2000@gmail.com>
2019-02-01 08:43:51 -0800

    application sending bakc json data

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ const express = require("express");
 
 const server = express();
 
+const games = [
+  {
+    title: "Pacman", // required
+    genre: "Arcade", // required
+    releaseYear: 1980 // not required
+  }
+];
+
 server.use(express.json());
 
 server.get("/", (req, res) => {
-  res.send(200);
+  res.sendStatus(200);
 });
 
 server.get("/games", (req, res) => {
-  res.send(200);
+  res.status(200).json(games);
 });
 
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -13,5 +13,11 @@ describe("SERVER: server.js", () => {
       let res = await request(server).get("/games");
       expect(res.status).toBe(200);
     });
+    it("should content type to be JSON", async () => {
+      let res = await request(server).get("/games");
+      console.log(res.data);
+
+      expect(res.type).toMatch(/json/i);
+    });
   });
 });
